6d10a2821d1d64676a7d95dbf614ae3d17dc6e74
cr1aftooret <cr1aftooret@gmail.com>
2024-06-13 19:04:29 +0800

    EEPROM

--- EEPROM/src/main.cpp
+++ EEPROM/src/main.cpp
@@ -4,47 +4,60 @@
 
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
-const byte BUTTON[] = {39, 35};
-int num = 0;
+int ll = 0;
+byte data;
+unsigned long long int num = 0;
 
-void display(int num);
+void display();
 
 void setup() {
-  EEPROM.begin(512);
+  EEPROM.begin(257);
   u8g2.begin();
-  for(byte i : BUTTON) {
-    pinMode(i, INPUT);
-  }
 
-  EEPROM.get(0, num);
+  ll = EEPROM.read(256);
 }
 
 void loop() {
-  for (int i = 0; i < 2; i++) {
-    if (digitalRead(BUTTON[i]) == false) {
-      //while (digitalRead(BUTTON[i]) == false);
-      switch (i) {
-        case 0:
-          num++;
-          break;
-        case 1:
-          num--;
-          break;
-      }
-      EEPROM.put(0, num);
+  display();
+
+
+  for (int i = 0; i <= ll; i++) {
+    data = EEPROM.read(i);
+
+    if (data < 255) {
+      EEPROM.write(i, data + 1);
+      EEPROM.commit();
+      break;
+    }
+
+    if (i == ll) {
+      EEPROM.write(i, 0);
+      EEPROM.commit();
+      ll++;
+      EEPROM.write(256, ll);
+      EEPROM.commit();
+      EEPROM.write(ll, 1);
+      EEPROM.commit();
+      break;
+    } else {
+      EEPROM.write(i, 0);
       EEPROM.commit();
     }
   }
-
-  display(num);
+  delay(1000);
 }
 
-void display(int num) {
+void display() {
   u8g2.setFont(u8g2_font_9x15_mf);
   u8g2.firstPage();
+
+  num = 0;
+  for (int i = 0; i <= ll; i++) {
+    num += EEPROM.read(i) * pow(256, i);
+  }
+
   do {
     u8g2.setCursor(64, 32);
-    u8g2.print(num);
-    
+    u8g2.print(String(num));
   } while (u8g2.nextPage());
 }
